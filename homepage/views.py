e423d29656f6e38045e4cfcfef7f7ab13dfd1225
Stephanie Hou <stephaniehou@cpe-68-174-37-123.nj.res.rr.com>
2023-02-26 14:20:35 -0500

    Cleaned Up Navigation

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-def index(request):
-    return render(request, "homepage/index.html")
+from django.urls import reverse
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+def index_view(request):
+    if not request.user.is_authenticated:
+        return render(request, 'homepage/index.html')
+    
+    return render(request, 'homepage/index.html', {
+        'welcome': 'Welcome {}!'.format(request.user.first_name)
+    }
+)
