9d76d96c0a61c8e048b8a9548f95708ee8615e01
ALFarouk Saleh <alfarouksaleh8@gmail.com>
2023-03-09 02:32:46 -0500

    Created the Markeplace Feature, Created Listing & Neighborhood Models, Created Add Listing & view Listing features as well

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Business
+from .models import Business, Listing, Neighborhood
 
 
 admin.site.register(Business)
+admin.site.register(Listing)
+admin.site.register(Neighborhood)
