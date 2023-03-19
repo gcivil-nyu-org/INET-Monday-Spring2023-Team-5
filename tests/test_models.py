e3ba940526ad25f4e8dada2e3413452631865a11
Weiye Sun <95750304+ws2309nyu@users.noreply.github.com>
2023-03-19 01:38:40 -0400

    updated code

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,14 +5,14 @@ from users.models import Business
 class BusinessTestCase(TestCase):
 
     def test_business_creation(self):
-        Business = Business.objects.create(name='Test Business Name', \
+        business = Business.objects.create(name='Test Business Name', \
                                            address='123 Test Ave', \
                                            owner='John Test', \
                                            email='jtest@gmail.com', \
                                            phone='123-456-7890'
                                            )
-        self.assertEqual(Business.name, 'Test Business Name')
-        self.assertEqual(Business.address, '123 Test Ave')
-        self.assertEqual(Business.owner, 'John Test')
-        self.assertEqual(Business.email, 'jtest@gmail.com')
-        self.assertEqual(Business.phone, '123-456-7890')
+        self.assertEqual(business.name, 'Test Business Name')
+        self.assertEqual(business.address, '123 Test Ave')
+        self.assertEqual(business.owner, 'John Test')
+        self.assertEqual(business.email, 'jtest@gmail.com')
+        self.assertEqual(business.phone, '123-456-7890')
