cb60dfc5235d0c113677cd6cbaff9ba8638bf52b
moringspeaker <guchenyu150533@gmail.com>
2023-04-01 17:54:58 -0400

    update my script

--- static/js/location.js
+++ static/js/location.js
@@ -5,10 +5,28 @@ window.onload = function () {
 function getPosition(){
  if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(function(res){
-   console.log("res",res)
+   var lat = res.coords.latitude;
+   var lon = res.coords.longitude;
+   // send two float values to the backend using a POST request
+    $.ajax({
+      url: '/',
+      type: 'POST',
+      data: {
+        value1: lat,
+        value2: lon
+      },
+      success: function(response) {
+        console.log('Success:', response);
+      },
+      error: function(xhr, status, error) {
+        console.error('Error:', error);
+      }
+    });
   },function(err){
    console.log("err",err)
   })
  }
 }
+
+
 
