cb87830b7543dd824368f2c0e430a8fcbe30a837
moringspeaker <guchenyu150533@gmail.com>
2023-04-03 16:05:36 -0400

    I perfected my JS script and now it can sent user's geographic information back to the backend

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-
+from scripts.location import get_neighborhood
 
 def index_view(request):
     if not request.user.is_authenticated:
         return render(request, "neighborhoods/index.html", {"page": "neighborhoods"})
+
+    if request.method == 'POST':
+        lat = request.POST.get('lat')
+        lon = request.POST.get('lon')
+        neighborhood = get_neighborhood(lat,lon)
+        neighborhood.lat = lat
+        neighborhood.lon = lon
+        neighborhood.save()
 
     return render(
         request,
         "neighborhoods/index.html",
         {"firstname": "{}".format(request.user.first_name), "page": "neighborhoods"},
     )
+
+
--- static/js/location.js
+++ static/js/location.js
@@ -1,32 +1,40 @@
-window.onload = function () {
-  //get user's location
-  getPosition();
+
+
+function runOnce(func) {// set this function to run only once
+  let hasRun = false;
+  return function() {
+    if (!hasRun) {
+      hasRun = true;
+      func.apply(this, arguments);
+    }
+  }
 }
-function getPosition(){
+
+
+const get_geo=function getPosition(){
  if(navigator.geolocation){
   navigator.geolocation.getCurrentPosition(function(res){
    var lat = res.coords.latitude;
    var lon = res.coords.longitude;
-   // send two float values to the backend using a POST request
-    $.ajax({
-      url: '/',
-      type: 'POST',
-      data: {
-        value1: lat,
-        value2: lon
-      },
-      success: function(response) {
-        console.log('Success:', response);
-      },
-      error: function(xhr, status, error) {
-        console.error('Error:', error);
-      }
-    });
+   var coor = {lat:lat,lon:lon};
+   var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
+   $.ajax({
+    url:"",
+    type:"post",
+
+    headers: { 'X-CSRFToken': csrftoken
+    },
+    data:{
+        coor:JSON.stringify(coor),
+    },
+    success:function(){
+        alert('Thanks for sharing your location!')
+    }
+    })
   },function(err){
    console.log("err",err)
   })
  }
 }
 
-
-
+window.onload = runOnce(get_geo);
