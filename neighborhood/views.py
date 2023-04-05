f3ea620645db95f5fb76309e06771e7419b39bca
Stephanie Hou <sh6403@nyu.edu>
2023-04-04 22:37:38 -0400

    Mapbox

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -11,15 +11,11 @@ from decouple import config
 def neighborhoods(request):
     neighborhoods = Neighborhood.objects.all()
 
-<<<<<<< HEAD
-    context = {"neighborhoods": neighborhoods, "page": "neighborhoods", "MAPBOX_API_KEY": os.environ.get('MAPBOX_API_KEY')}
-=======
     context = {
         "neighborhoods": neighborhoods,
         "page": "neighborhoods",
         "MAPBOX_API_KEY": config("MAPBOX_API_KEY"),
     }
->>>>>>> fba1830f246c0087a6d34695bce4705065b984a9
 
     if request.user.is_authenticated:
         context["firstname"] = request.user.first_name
