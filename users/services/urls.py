1ff0188d35f07f82d47108b8246f46b313bf7f7b
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2023-04-19 17:46:21 -0400

    Create profile page for user account
    This commit will add the view which shows the user's listings
    and businesses on the account page. It also adds a view for
    showing user's listings.

--- users/services/urls.py
+++ users/services/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from users.services import views
 
+
 urlpatterns = [
     path("", views.businesses, name="user_businesses"),
     path("add/", views.add, name="add_business"),
