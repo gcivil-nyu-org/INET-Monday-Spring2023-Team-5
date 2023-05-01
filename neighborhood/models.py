eaa5071c978a3677440746e8c0e0d75f94503e4e
Venu Vardhan Reddy Tekula <vt2182@nyu.edu>
2023-04-30 22:28:54 -0400

    Add crime & population data to the neighborhood fixtures
    Co-authored-by: moringspeaker <guchenyu150533@gmail.com>

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -7,9 +7,18 @@ class Neighborhood(models.Model):
     description = models.TextField()
     lat = models.FloatField()
     lon = models.FloatField()
+    population = models.PositiveIntegerField(null=True)
+    crimes = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return self.name
 
     def get_geopoint(self):
         return "POINT(%s %s)" % (self.lon, self.lat)
+
+    def crime_rate(self):
+        if self.population and self.crimes:
+            crime_rate = self.crimes / self.population * 100
+            return "{:.2%}".format(crime_rate)
+        else:
+            return "N/A"
